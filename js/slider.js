09fb9e76447e83b37fdc54dae8a41e28ee5ac060
sabimonica <yulitssasabirojasm18@gmail.com>
2023-05-10 19:50:16 -0500

    imagenes siguientes

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,55 @@
-let leftArrow = document.getElementById("leftArrow");
-let imgToyota = document.getElementById("imgToyota")
+let bntnet = document.querySelector('#btnNex');
+let btnbefore = document.querySelector('#btnBefore');
+let contimg = document.querySelector('#imgCont');
+contador = 3;
+function sumar() {
+    contador++
+    if (contador == 4) {
+        contador =1;
+    }
+    anime();
+}
+function restar() {
+    contador--
+    if (contador == 0) {
+        contador = 3;
+    }
+    anime();
+}
 
-leftArrow.addEventListener("click", function(){
-    leftArrow.classList.remove("img-toyota-camry");
-    imgToyota.classList.add("img-toyota-camry-hidden");
-});
+function anime() {
+    if (contimg.classList.contains('animation')) {
+        contimg.classList.add('animation2');
+        contimg.classList.remove('animation');
+    }else{
+        contimg.classList.remove('animation2');
+        contimg.classList.add('animation');
+    }
+    setTimeout(() => {
+        proyectar();
+    }, 500);
+}
+function proyectar() {
+    if (contador == 3) {
+        img1();
+    }
+    if (contador == 1) {
+        img2();
+    }
+    if (contador == 2) {
+        ima3();
+    }
+}
+function img1() {
+    contimg.setAttribute('src', ('img/img2.jpg'));
+}
+function img2() {
+    contimg.setAttribute('src', ('img/img3.jpg'));
+}
+function ima3() {
+    contimg.setAttribute('src', ('img/img4.jpg'));
+}
+
+
+btnbefore.addEventListener('click', restar);
+bntnet.addEventListener('click', sumar);
