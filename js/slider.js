bd0ce8d7917b26eb50360cc33b44216f0a472a2c
sabimonica <yulitssasabirojasm18@gmail.com>
2023-05-17 20:27:10 -0500

    slider-finalizado

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,45 @@
-let lefArrow = document.getElementById("leftAroow");
+let leftAdelante = document.getElementById("leftAdelante");
+let leftAroow = document.getElementById("leftAroow");
 let imgToyotaCamry = document.getElementById("imgToyotaCamry");
 let imgToyotaTundra = document.getElementById("imgToyotaTundra");
 let imgToyotaRav4 = document.getElementById('imgToyotaRav4');
-let imgToyotaFlower = document.getElementById("imgToyotaFlower");
+// let imgToyotaFlower = document.getElementById("imgToyotaFlower");
 
 
 
-lefArrow.addEventListener("click", function () {
+leftAdelante.addEventListener("click", function () {
     if (imgToyotaCamry.classList.contains("img-toyota-camry")) {
         imgToyotaCamry.classList.remove("img-toyota-camry");
         imgToyotaCamry.classList.add("img-toyota-camry-hidden");
-        imgToyotaTundra.classList.remove("img-toyota-tundra");
+        imgToyotaTundra.classList.add("img-toyota-tundra");
         imgToyotaTundra.classList.remove("img-toyota-tundra-hidden-right");
-        imgToyotaRav4.classList.add("img-toyota-rav4");
-        imgToyotaRav4.classList.remove("img-toyota-rav4-hidden-right");
-
-        
-
-
+        // imgToyotaRav4.classList.add("img-toyota-rav4");
+        // imgToyotaRav4.classList.remove("img-toyota-rav4-hidden-right");
+        console.log("UNO")
     }
-    else if (imgToyotaRav4.classList.contains("img-toyota-rav4")) {
-        imgToyotaRav4.classList.remove("img-toyota-rav4");
-        imgToyotaRav4.classList.add("img-toyota-rav4-hidden-right");
-        imgToyotaFlower.classList.add("img-toyota-flower");
-        imgToyotaFlower.classList.remove("img-toyota-flower-hidden-right");
-        
-       
+    else if (imgToyotaRav4.classList.contains("img-toyota-rav4-hidden-right")) {
+        imgToyotaTundra.classList.remove("img-toyota-tundra");
+        imgToyotaTundra.classList.add("img-toyota-tundra-hidden-left");
+        imgToyotaRav4.classList.remove("img-toyota-rav4-hidden-right")
+        imgToyotaRav4.classList.add("img-toyota-rav4")
+        console.log("DOS")
+    }
+
+});
+leftAroow.addEventListener("click", function () {
+    if (imgToyotaRav4.classList.contains("img-toyota-rav4")) {
+        imgToyotaTundra.classList.add("img-toyota-tundra");
+        imgToyotaTundra.classList.remove("img-toyota-tundra-hidden-left");
+        imgToyotaRav4.classList.add("img-toyota-rav4-hidden-right")
+        imgToyotaRav4.classList.remove("img-toyota-rav4")
+        console.log("UNO T")
+    }
+    else if (imgToyotaRav4.classList.contains("img-toyota-rav4-hidden-right")) {
+        imgToyotaCamry.classList.add("img-toyota-camry");
+        imgToyotaCamry.classList.remove("img-toyota-camry-hidden");
+        imgToyotaTundra.classList.remove("img-toyota-tundra");
+        imgToyotaTundra.classList.add("img-toyota-tundra-hidden-right");
+        console.log("DOS T")
     }
 
 });
