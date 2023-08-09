2f46a8e6ba16eb6107c9c50c703d1f99f79a6d49
sabimonica <yulitssasabirojasm18@gmail.com>
2023-08-09 11:37:20 -0500

    cambion en homecenter

--- js/homecenter.js
+++ js/homecenter.js
@@ -1,6 +1,7 @@
 let idLupa = document.getElementById("idLupa");
 let boxHeader1 = document.getElementById("boxHeader1");
 let boxHeader2 = document.getElementById("boxHeader2");
+let idExit = document.getElementById("idExit");
 
 idLupa.addEventListener("click",function(){
     boxHeader1.classList.add("box-header1-hidden");
@@ -8,3 +9,10 @@ idLupa.addEventListener("click",function(){
     boxHeader2.classList.remove("box-header-2-hidden");
     boxHeader2.classList.add("box-header-2")
 });
+idExit.addEventListener("click",function(){
+    boxHeader1.classList.remove("box-header1-hidden");
+    boxHeader1.classList.add("box-header-1");
+    boxHeader2.classList.add("box-header-2-hidden");
+    boxHeader2.classList.remove("box-header-2")
+});
+
