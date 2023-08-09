c2e1e72bf054ee85a2e56eee4432abea8a9d02ec
sabimonica <yulitssasabirojasm18@gmail.com>
2023-08-09 11:13:50 -0500

    arreglos de homecenter

--- js/homecenter.js
+++ js/homecenter.js
@@ -5,6 +5,6 @@ let boxHeader2 = document.getElementById("boxHeader2");
 idLupa.addEventListener("click",function(){
     boxHeader1.classList.add("box-header1-hidden");
     boxHeader1.classList.remove("box-header-1");
-    boxHeader2.classList.remove("box-header2-hidden");
-    boxHeader2.classList.add("box-header-2");
+    boxHeader2.classList.remove("box-header-2-hidden");
+    boxHeader2.classList.add("box-header-2")
 });
