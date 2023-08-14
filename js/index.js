e369eda01122863f4be2aee12803b9ce59e122ad
sabimonica <yulitssasabirojasm18@gmail.com>
2023-08-14 17:35:53 -0500

    realizacion de portafolio

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function mostrarDetalleProyecto(proyecto) {
+  const detalleProyecto = proyecto.querySelector('.detalle-proyecto');
+  detalleProyecto.style.display = detalleProyecto.style.display === 'block' ? 'none' : 'block';
+}
--- js/portafolio.js
+++
@@ -1,9 +0,0 @@
-// Agregar interacción a los proyectos
-document.getElementById("project1").addEventListener("click", () => {
-    alert("Haz hecho clic en Proyecto 1");
-  });
-  
-  document.getElementById("project2").addEventListener("click", () => {
-    alert("Haz hecho clic en Proyecto 2");
-  });
-  
